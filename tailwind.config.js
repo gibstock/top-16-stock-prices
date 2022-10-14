a3bc96228cf5d787f3124e364636f44d15e45369
Andre Gonzales <gibstock@gmail.com>
2022-10-13 21:47:51 -0700

    Made header feature, added color scheme, added darkmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Light
+          // Primary-5%
+          // Gray-11
+          // Gray-8
+          // Color-2
+          // BG-2
+        // Dark
+          // Gray-11
+          // Gray-8
+          // Color-2
+          // BG-2
+          // Gray-0
+        light: {
+          primary5: '#f3f8ff',
+          gray11: '#000000',
+          gray8: '#616161',
+          color2: '#049c6b',
+          bg2: '#e8ffeb'
+        },
+        dark: {
+          primary5: '#01060d',
+          gray11: '#fefefe',
+          gray8: '#efefef',
+          color2: '#07f8b5',
+          bg2: '#2a4037'
+        }
+      }
+    },
   },
   plugins: [],
+  darkMode: 'class'
 }
