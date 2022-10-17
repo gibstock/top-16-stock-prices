78397b47e3db16a7ae6ee8651254ff05d17c9b07
Andre Gonzales <gibstock@gmail.com>
2022-10-17 09:26:03 -0700

    made grid

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from 'next'
 import { Header } from '../components/header/header'
+import { MainStage } from '../components/mainStage/mainStage'
 
 const Home: NextPage = () => {
   return (
     <div className="h-screen w-full p-6 bg-light-primary5 dark:bg-dark-primary5">
         <Header />
+        <MainStage />
     </div>
   )
 }
