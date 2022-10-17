3d5826466810032e00b2b4c4f3006595e439e02f
Andre Gonzales <gibstock@gmail.com>
2022-10-17 08:52:27 -0700

    added padding to main document

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import { Header } from '../components/header/header'
 
 const Home: NextPage = () => {
   return (
-    <div className="h-screen w-full bg-light-primary5 dark:bg-dark-primary5">
+    <div className="h-screen w-full p-6 bg-light-primary5 dark:bg-dark-primary5">
         <Header />
     </div>
   )
