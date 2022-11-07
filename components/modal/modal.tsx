00ba64729797509f335296054bc2a24ed3690bc8
Andre Gonzales <gibstock@gmail.com>
2022-11-07 15:56:18 -0800

    Added close button to modal

--- components/modal/modal.tsx
+++ components/modal/modal.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useNewQuoteStore } from '../../store/store'
 import { useSymbolData } from '../../hooks/useStockData'
 import { binarySearch } from '../../utils/binarySearch'
+import {AiOutlineClose} from 'react-icons/ai'
 
 type AppProps = {
   curr: string;
@@ -61,16 +62,17 @@ export const Modal = ({curr, setCurrentSymbol, setTickerSymbols, tickerSymbols, 
 
   return (
     <>
-      <div className='fixed flex flex-col justify-center items-center p-2 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-1/2  rounded-lg shadow-[0px_1px_4px] shadow-light-shadow z-10 dark:shadow-dark-shadow'>
-        <form className='flex flex-col justify-center items-center' onSubmit={handleAddQuoteClick}>
+      <div className='fixed flex flex-col justify-center items-center p-2 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  rounded-lg  z-10'>
+        <div className="close-btn absolute -top-2 -right-2 cursor-pointer outline outline-1 outline-black bg-light-primary5 rounded-full" onClick={() => setLaunchModal(false)}><AiOutlineClose size={24} /></div>
+        <form className='flex flex-col justify-center items-center ' onSubmit={handleAddQuoteClick}>
           <input 
-            className=' text-xl  px-3 py-2 rounded-xl'
+            className=' text-xl  px-3 py-2 rounded-xl shadow-[0px_1px_4px] shadow-light-shadow  dark:shadow-dark-shadow'
             name='newQuote'
             type='text' 
             placeholder={curr}
             autoFocus={true}
           />
-          <button className='bg-blue-500 px-3 py-2 mt-2 rounded-full w-full text-light-primary5 font-bold text-xl' type='submit'>Add Quote</button>
+          <button className='bg-blue-500 px-3 py-2 mt-2 rounded-full w-full text-light-primary5 font-bold text-xl shadow-[0px_1px_4px] shadow-light-shadow  dark:shadow-dark-shadow' type='submit'>Add Quote</button>
         </form>
       </div>
         <div className="absolute left-0 top-0 bg-black opacity-30 h-screen w-screen"></div>
